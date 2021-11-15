43288ff48dbe85ac256be88f3d286fa5847a3069
Jacklyn Malone <jjmlmhc.09@gmail.com>
2021-11-15 14:50:41 -0500

    added tables for schema file

--- db/schema.sql
+++ db/schema.sql
@@ -1,18 +1,33 @@
-DROP DATABASE IF EXISTS movies_db;
-CREATE DATABASE movies_db;
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
 
-USE movies_db;
+USE employees_db;
 
-CREATE TABLE movies (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  movie_name VARCHAR(100) NOT NULL
+DROP TABLE IF EXISTS department;
+CREATE TABLE department (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE reviews (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    movie_id INT,
-    review TEXT NOT NULL,
-    FOREIGN KEY (movie_id)
-    REFERENCES movies(id)
-    ON DELETE SET NULL
+DROP TABLE IF EXISTS roles;
+CREATE TABLE roles (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   title VARCHAR(30) NOT NULL,
+	salary DECIMAL,
+	department_id INT NOT NULL,
+	FOREIGN KEY (department_id),
+   REFERENCES department(id)
+);
+
+DROP TABLE IF EXISTS employee;
+CREATE TABLE employee (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+   first_name VARCHAR(30) NOT NULL,
+	last_name VARCHAR(30) NOT NULL,
+	role_id INT NOT NULL,
+	manager_id INT NOT NULL,
+	FOREIGN KEY (role_id),
+   REFERENCES roles(id),
+	FOREIGN KEY (manager_id),
+	REFERENCES employee(id),
 );
