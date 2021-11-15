594bb90bc6f43b429931c6e3850448a00c54e6c2
Jacklyn Malone <jjmlmhc.09@gmail.com>
2021-11-15 15:39:06 -0500

    added seeds file

--- db/schema.sql
+++ db/schema.sql
@@ -6,7 +6,7 @@ USE employees_db;
 DROP TABLE IF EXISTS department;
 CREATE TABLE department (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	name VARCHAR(30) NOT NULL
+	dep_name VARCHAR(30) NOT NULL
 );
 
 DROP TABLE IF EXISTS roles;
@@ -25,7 +25,7 @@ CREATE TABLE employee (
    first_name VARCHAR(30) NOT NULL,
 	last_name VARCHAR(30) NOT NULL,
 	role_id INT NOT NULL,
-	manager_id INT NOT NULL,
+	manager_id INT,
 	FOREIGN KEY (role_id),
    REFERENCES roles(id),
 	FOREIGN KEY (manager_id),
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,16 +1,25 @@
-INSERT INTO movies (movie_name)
-VALUES ("Lion King"),
-       ("The Godfather"),
-       ("West Side Story"),
-       ("Parasite"),
-       ("The Wizard of Oz");
+INSERT INTO department (dep_name)
+VALUES ("Sales"),
+		("Engineering"),
+      ("Finance"),
+      ("Legal");
 
-INSERT INTO reviews (movie_id, review)
-VALUES (1, "Zazu is underrated. Give that hornbill a sequel!"),
-       (2, "I'm gonna make him an offer you can't refuse, watch this movie"),
-       (1, "Scar is the lion everyone loves to hate"),
-       (3, "Ten years of ballet and three years of tap to join a gang in this neighborhood"),
-       (5, "The tin man gave a metallic, hollow performance"),
-       (1, "Hakuna matata"),
-       (5, "Those flying monkeys are nightmare fuel!");
-		 
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Salesperson", 80000, 1),
+      ("Lead salesperson", 90000, 1),
+      ("Lead Engineer", 150000, 2),
+      ("Software Engineer", 120000, 2),
+      ("Account Manager", 160000, 3),
+      ("Accountant", 125000, 3),
+      ("Lead Lawyer", 250000, 4),
+		("Lawyer", 180000, 4);
+
+INSERT INTO roles (first_name, last_name, role_id, manager_id)
+VALUES ("Mike", "Chan", 1, 1),
+      ("Ashley", "Rodriquez", 2, NULL),
+      ("Kevin", "Tupik", 3, NULL),
+      ("Kunal", "Singh", 4, 2),
+      ("Malia", "Brown", 5, NULL),
+      ("Sarah", "Lourd", 6, 3),
+      ("Tom", "Allen", 7, NULL),
+		("Barry", "Bonds", 8, 4);
